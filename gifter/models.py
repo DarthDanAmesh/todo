4e4e04124eb93fcca6380c75b7a42a3188c3b52e
Dan Amemba <danielamemba@gmail.com>
2022-06-09 06:41:55 -0400

    created model for gifter app

--- gifter/models.py
+++ gifter/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+class Gift(models.Model):
+    class Gifttype(models.TextChoices):
+        HOLIDAY = "HOLIDAY", "Holiday"
+        ANNIVERSARY = "ANNIVERSARY", "Anniversary"
+        BIRTHDAY = "BIRTHDAY", "Birthday"
+
+    name = models.CharField(max_length=200)
+    event = models.CharField(choices=Gifttype.choices, max_length=30)
+    date_assigned = models.DateTimeField(auto_now_add=True)
+    due_date = models.DateTimeField()
+    status = models.IntegerField(STATUS, default=0)
+
+    def __str__(self):
+        return self.name
